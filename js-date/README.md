9c4a2e1fa96ee9152ee865c483ee7c1bbce0b153
wdssmq <wdssmq@qq.com>
2021-12-06 22:14:39 +0800

    add js-date

---
+++ js-date/README.md
@@ -0,0 +1,14 @@
+## JavaScript 时间对象
+
+时间相关的笔记及函数
+
+### getDay() 方法
+
+`getDay()` 方法返回指定日期是星期几（从 0 到 6）。
+
+注释：星期日为 0，星期一为 1，依此类推。
+
+```js
+const oDate = new Date();
+console.log(oDate.getDay());
+```
---
+++ js-date/date.js
@@ -0,0 +1,39 @@
+(function () {
+  // "2021-12-06" 转 Date 对象
+  function fnStrToDate(str) {
+    const arr = str.split("-");
+    const date = new Date(arr[0], arr[1] - 1, arr[2]);
+    return date;
+  }
+  // Date 对象格式化为 "2021-12-06"
+  function fnFormatDate(oDate) {
+    const sDate = oDate.getDate();
+    if (sDate < 10) {
+      sDate = "0" + sDate;
+    }
+    const sMonth = oDate.getMonth() + 1;
+    if (sMonth < 10) {
+      sMonth = "0" + sMonth;
+    }
+    const sYear = oDate.getFullYear();
+    return sYear + "-" + sMonth + "-" + sDate;
+  }
+  // 获取日期所在周的星期一
+  function fnGetCurMonday(dt = new Date()) {
+    if (typeof dt === "string") {
+      dt = fnStrToDate(dt);
+    }
+    dt.setDate(dt.getDate() - dt.getDay() + 1);
+    return fnFormatDate(dt);
+  }
+  // 获取日期所在周的星期日
+  function fnGetCurSunday(dt = new Date()) {
+    if (typeof dt === "string") {
+      dt = fnStrToDate(dt);
+    }
+    dt.setDate(dt.getDate() - dt.getDay() + 7);
+    return fnFormatDate(dt);
+  }
+  // --------------
+  console.log(fnGetCurMonday("2021-12-04"));
+})();
