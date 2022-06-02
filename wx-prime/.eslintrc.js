20862975cc7e865fcfecce463c24da364f1b9412
wdssmq <wdssmq@qq.com>
2022-06-02 19:07:03 +0800

    chore. eslint 配置；

--- wx-prime/.eslintrc.js
+++ wx-prime/.eslintrc.js
@@ -27,5 +27,14 @@ module.exports = {
     requireMiniProgram: true,
   },
   // extends: 'eslint:recommended',
-  rules: {},
-}
+  rules: {
+    "arrow-parens": 0,
+    "generator-star-spacing": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": [1, "always-multiline"],
+    "no-unused-vars": [1, { "args": "none" }],
+    "spaced-comment": [2, "always"],
+    "semi": [2, "always", { "omitLastInOneLineBlock": true }],
+    "quotes": [2, "double"],
+    // "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,},
+  }
