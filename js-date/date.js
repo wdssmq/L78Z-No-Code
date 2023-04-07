7bc3200b9eaeaf318bc2d83ccc1cda6d8304b924
wdssmq <wdssmq@qq.com>
2023-04-07 12:15:52 +0800

    style. js-date/date.js

--- js-date/date.js
+++ js-date/date.js
@@ -5,6 +5,7 @@
     const date = new Date(arr[0], arr[1] - 1, arr[2]);
     return date;
   }
+
   // Date 对象格式化为 "2021-12-06"
   function fnFormatDate(oDate) {
     let sDate = oDate.getDate();
@@ -18,6 +19,7 @@
     const sYear = oDate.getFullYear();
     return sYear + "-" + sMonth + "-" + sDate;
   }
+
   // 获取日期所在周的星期一
   function fnGetCurMonday(dt = new Date()) {
     if (typeof dt === "string") {
@@ -26,6 +28,7 @@
     dt.setDate(dt.getDate() - dt.getDay() + 1);
     return fnFormatDate(dt);
   }
+
   // 获取日期所在周的星期日
   function fnGetCurSunday(dt = new Date()) {
     if (typeof dt === "string") {
@@ -34,6 +37,7 @@
     dt.setDate(dt.getDate() - dt.getDay() + 7);
     return fnFormatDate(dt);
   }
+
   // --------------
   console.log(fnGetCurMonday("2021-12-04"));
 })();
