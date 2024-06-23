bfbbe2ed2b0ce971542a6c6d4f199c4d8b4e35aa
wdssmq <wdssmq@qq.com>
2024-06-23 22:57:07 +0800

    up-bt2mag. 打包时需要指定 base 参数

--- php-Tools/_dev_bt2mag/astro.config.mjs
+++ php-Tools/_dev_bt2mag/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+    base: '/tools/bt2mag',
+    trailingSlash: 'never',
     integrations: [
         tailwind(),
     ],
