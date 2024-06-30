626fea492ed92444489ab0ea5c5a77928848c072
wdssmq <wdssmq@qq.com>
2024-06-30 18:31:06 +0800

    chore. 使用 env.config.mjs 控制实际配置项

--- php-Tools/_dev_bt2mag/astro.config.mjs
+++ php-Tools/_dev_bt2mag/astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
 
+import curEnv from './env.config.mjs'
+
 // https://astro.build/config
 export default defineConfig({
-    base: '/test/bt2mag',
+    base: curEnv.base || '/',
     trailingSlash: 'never',
+    outDir: curEnv.outDir || 'dist',
     integrations: [
         tailwind(),
     ],
